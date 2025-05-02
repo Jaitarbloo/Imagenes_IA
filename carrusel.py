7f21d9c744b9a25dadd5bfa63d2a470d804f1329
Jaitarbloo <64631993+Jaitarbloo@users.noreply.github.com>
2025-05-02 11:52:29 +0200

    Funciona OK

--- carrusel.py
+++ carrusel.py
@@ -1,10 +1,35 @@
 import reflex as rx
-from state import State
+import asyncio
 
-def carrusel_imagenes():
+class State(rx.State):
+    imagenes: list[str] = [
+        "/concorde 1.jpg",
+        "/concorde 2.jpg", 
+        "/concorde 3.jpg",
+        "/concorde 7.jpg"
+    ]
+    indice_actual: int = 0
+
+    async def auto_slider(self):
+        while True:
+            await asyncio.sleep(3)
+            self.indice_actual = (self.indice_actual + 1) % len(self.imagenes)
+            yield
+
+def index():
     return rx.box(
-                    rx.image(src=State.imagenes[State.indice_actual],
-                    width="800px", 
-                    height="400px"),
-            margin_top="8em"
-                ) 
+        rx.image(
+            src=State.imagenes[State.indice_actual],
+            width="800px",
+            height="400px"
+        ),
+        display="flex",
+        justify="center",
+        align="center",
+        height="100vh",
+        width="100vw",
+        margin_top="0"
+    )
+
+app = rx.App()
+app.add_page(index, on_load=State.auto_slider)
