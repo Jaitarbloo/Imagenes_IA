2fb619deeaaee3a7bcc5a349bb676908bc1f4a76
Jaitarbloo <64631993+Jaitarbloo@users.noreply.github.com>
2025-05-02 10:11:22 +0200

    Update ampliacion_fotos.py

--- ampliacion_fotos.py
+++ ampliacion_fotos.py
@@ -1,66 +1,49 @@
 import reflex as rx
-from state import State
 
-def Ampliacion_fotos():
-    return rx.box(
-                rx.hstack(
-                    rx.link(
-                        rx.box(
-                            rx.image("/coche.jpg",height="100%",border_radius="7px"),
-                        position="absolute",  # Posición absoluta
-                        left="100px",  # Posición fija desde la izquierda
-                        width=rx.cond(
-                        State.expanded_coche,
-                        "400px",
-                        "250px"),
-                        height=rx.cond(
-                        State.expanded_coche,
-                        "400px", 
-                        "250px"),
-                        align="center",
-                        on_mouse_enter=State.toggle_size_coche,
-                        on_mouse_leave=State.toggle_size_coche,
-                        transition="all 1s ease-in-out",
-                        z_index=rx.cond(  # z-index condicional
-                            State.expanded_coche,
-                            "2",
-                            "1"
-                                        )
-                            ),
-                         ),
-                    
-                    #rx.spacer(),
+class State(rx.State):
+    expanded_coche: bool = False
+    expanded_bici: bool = False
 
-                    rx.link(
-                        rx.box(
-                            rx.image("/bici.jpg",height="100%",border_radius="7px"),
-                        position="absolute",  # Posición absoluta
-                        left="400px",  # Posición fija desde la izquierda
-                        width=rx.cond(
-                        State.expanded_bici,
-                        "400px",
-                        "250px"),
-                        height=rx.cond(
-                        State.expanded_bici,
-                        "400px", 
-                        "250px"),
-                        align="center",
-                        on_mouse_enter=State.toggle_size_bici,
-                        on_mouse_leave=State.toggle_size_bici,
-                        transition="all 1s ease-in-out",
-                        z_index=rx.cond(  # z-index condicional
-                            State.expanded_bici,
-                            "2",
-                            "1"
-                                        )
-                            ),
-                         ),    
-            width="100%",
-            height="50vh",
-            padding_top="2em",
-            spacing="2"
+    def toggle_size_coche(self):
+        self.expanded_coche = not self.expanded_coche
+
+    def toggle_size_bici(self):
+        self.expanded_bici = not self.expanded_bici
+
+def index():
+    return rx.flex(
+        rx.box(
+            rx.image(
+                src="/coche.jpg",
+                border_radius="7px",
+                width=rx.cond(State.expanded_coche, "400px", "250px"),
+                height=rx.cond(State.expanded_coche, "400px", "250px"),
+                transition="all 0.5s ease-in-out",
             ),
-            position="relative",  # Contenedor con posición relativa
+            on_mouse_enter=State.toggle_size_coche,
+            on_mouse_leave=State.toggle_size_coche,
+            align="center",
+            justify="center",
+        ),
+        rx.box(
+            rx.image(
+                src="/bici.jpg",
+                border_radius="7px",
+                width=rx.cond(State.expanded_bici, "400px", "250px"),
+                height=rx.cond(State.expanded_bici, "400px", "250px"),
+                transition="all 0.5s ease-in-out",
+            ),
+            on_mouse_enter=State.toggle_size_bici,
+            on_mouse_leave=State.toggle_size_bici,
+            align="center",
+            justify="center",
+        ),
+        spacing="4",
+        justify="center",
+        align="center",
         width="100%",
-        height="100vh"
-    ) 
+        height="70vh",
+    )
+
+app = rx.App()
+app.add_page(index)
